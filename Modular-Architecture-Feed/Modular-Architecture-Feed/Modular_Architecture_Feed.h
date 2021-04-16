12b3d2f2aca47ad95d6c97b51742d4288ff157e1
Trevor Whittingham <twhittingham@ancestry.com>
2021-04-16 12:28:32 -0600

    created Xcode project

---
+++ Modular-Architecture-Feed/Modular-Architecture-Feed/Modular_Architecture_Feed.h
@@ -0,0 +1,18 @@
+//
+//  Modular_Architecture_Feed.h
+//  Modular-Architecture-Feed
+//
+//  Created by Trevor Whittingham on 4/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Modular_Architecture_Feed.
+FOUNDATION_EXPORT double Modular_Architecture_FeedVersionNumber;
+
+//! Project version string for Modular_Architecture_Feed.
+FOUNDATION_EXPORT const unsigned char Modular_Architecture_FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Modular_Architecture_Feed/PublicHeader.h>
+
+
